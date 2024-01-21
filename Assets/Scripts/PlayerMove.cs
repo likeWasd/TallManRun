c3019025b64e29acb93cdc1265878da593301388
likeWasd <mana.dog0077@gmail.com>
2024-01-21 13:09:18 +0900

    キャラクターを自動で前進させて、Aキーで左に移動するようにした

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * 0.1f;
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= transform.right * 0.1f;
+        }
+    }
+}
